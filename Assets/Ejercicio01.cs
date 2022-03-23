527d82a87fa2af06aa466a38acebce07908ecdbe
soficasares <102155698+soficasares@users.noreply.github.com>
2022-03-23 15:55:49 -0300

    Exercise 1 finished

--- Assets/Ejercicio01.cs
+++ Assets/Ejercicio01.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Ejercicio01 : MonoBehaviour
 {
+    int num1 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1*num1); 
     }
 
     // Update is called once per frame

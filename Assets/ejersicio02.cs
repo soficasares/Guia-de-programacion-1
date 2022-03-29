2b7441e4e6272db2f728e3539abaac73618991c2
soficasares <102155698+soficasares@users.noreply.github.com>
2022-03-29 08:34:14 -0300

    .

---
+++ Assets/ejersicio02.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejersicio02 : MonoBehaviour {
+
+
+    int num1 = 5;
+    int num2 = 6;
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log(num1 + num2);
+        Debug.Log(num1 * num2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

7f567728810ee6f7b70bc8da8d412ead743a951d
soficasares <102155698+soficasares@users.noreply.github.com>
2022-04-05 08:54:02 -0300

    exersice 06 finished

---
+++ Assets/ejersicio06.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejersicio06 : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 % 2 == 1)
+        {
+            Debug.Log("el numero es impar");
+        }
+        else if (num1 % 2 == 0)
+        {
+            Debug.Log("el numero es par");
+        }
+        else
+        {
+            Debug.Log("error: el valor no es mayor a 0");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
